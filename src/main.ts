e9ab35373b31197b890719b334bc81d04ca9ea0e
ellidall <124914524+ellidall@users.noreply.github.com>
2025-03-31 10:42:21 +0300

    работает генератор, но фиг знает правильно или нет

--- src/main.ts
+++ src/main.ts
@@ -20,9 +20,9 @@ const parseGrammar = (grammar: string[]): TableRow[] => {
 
     // первые строки - инициализация
     for (const rule of grammar) {
-        const [production, guidingPart] = rule.split(" / ")
-        const [leftSide, rightSide] = production.split(" -> ")
-        const guidingSymbols = new Set(guidingPart.split(", ").map(s => s.trim()))
+        const [production, guidingPart] = rule.split(' / ')
+        const [leftSide, rightSide] = production.split(' -> ')
+        const guidingSymbols = new Set(guidingPart.split(', ').map(s => s.trim()))
         const symbols = rightSide.match(/<[^>]+>|[^<>\s]+/g) || []
         const row: TableRow = {
             number: lineNumber,
@@ -33,7 +33,7 @@ const parseGrammar = (grammar: string[]): TableRow[] => {
             pointer: null,
             stackPushIndex: null,
             isParsingEnd: false,
-            rightSide: symbols
+            rightSide: symbols,
         }
         initialTable.push(row)
         table.push(row)
@@ -49,13 +49,13 @@ const parseGrammar = (grammar: string[]): TableRow[] => {
     // строки до конца
     for (const row of initialTable) {
         for (const symbol of row.rightSide) {
-            const equals = initialTable.filter(row => row.symbol ===  symbol)
+            const equals = initialTable.filter(row => row.symbol === symbol)
             table.push({
                 number: lineNumber,
                 symbol: symbol,
-                guidingSymbols: symbol[1] === "<"
+                guidingSymbols: symbol[1] === '<'
                     ? new Set(...equals.map(row => row.guidingSymbols.values()))
-                    : symbol !== "e"
+                    : symbol !== 'e'
                         ? new Set(symbol)
                         : new Set(),
                 isError: true,
@@ -63,7 +63,7 @@ const parseGrammar = (grammar: string[]): TableRow[] => {
                 pointer: null,
                 stackPushIndex: null,
                 isParsingEnd: false,
-                rightSide: null
+                rightSide: null,
             })
             lineNumber++
         }
@@ -71,17 +71,17 @@ const parseGrammar = (grammar: string[]): TableRow[] => {
 
     // замена e в left и в направляющих множествах
     for (const row of table) {
-        let t: string[]
-        if (row.guidingSymbols.values()[0] === "e" || row.symbol === "e") {
+        let t: string[] = []
+        if (Array.from(row.guidingSymbols.values())[0] === 'e' || row.symbol === 'e') {
             const left = row.symbol
             for (const row of initialTable) {
                 for (let i = 0; i < row.rightSide.length; i++) {
                     if (row.rightSide[i] === left) {
                         if (i + 1 < row.rightSide.length) {
-                            t.push(row.rightSide[i+1])
+                            t.push(row.rightSide[i + 1])
 
                         } else {
-                            t.push("#")
+                            t.push('#')
                         }
                     }
                 }
@@ -92,49 +92,71 @@ const parseGrammar = (grammar: string[]): TableRow[] => {
 
     // флаг ошибки
     for (let i = 0; i < lineNumber; i++) {
-        if (i >= lineNumber-1) {
+        if (i >= lineNumber - 1) {
             break
         }
-        if (table[i].symbol === table[i+1].symbol) {
+        if (!table[i] || !table[i + 1]) {
+            console.error(`Ошибка: table[${i}] или table[${i + 1}] равно undefined`);
+            continue;
+        }
+        if (table[i].symbol === table[i + 1].symbol) {
             table[i].isError = false
         }
     }
 
     // указатель
-    for (const [nonTerminal, lines] of nonTerminalMap.entries()) {
+    for (const [nonTerminal, lines] of Array.from(nonTerminalMap.entries())) {
         for (let i = 0; i < lines.length; i++) {
             const index = lines[i] - 1
-            table[index].pointer = lines[0] // Указываем на первую альтернативу
+            table[index].pointer = lines[0]
         }
     }
 
 
+    // стек
+    for (const row of table) {
+        if (row.number < initialTable.length) {
+            row.stackPushIndex = null
+        } else {
+            if (row.symbol[0] == '<') {
+                for (const initialRow of initialTable) {
+                    if (row.symbol == initialRow.symbol) {
+                        row.stackPushIndex = initialRow.number
+                    }
+                }
+            } else {
+                row.stackPushIndex = row.number + 1
+            }
+        }
+    }
+
+    // конец
+    for (const row of table) {
+        row.symbol == '#' ? row.isParsingEnd == true : row.isParsingEnd == false
+    }
 
     return table
-};
+}
 
 const main = () => {
     const grammarSample = [
-        "<S> -> <E># / id, -, (",
-        "<E> -> <T><E'1> / id, -, (",
-        "<T> -> <F><T'1> / id, -, (",
-        "<F> -> id / id",
-        "<F> -> -<F> / -",
-        "<F> -> (<F>) / (",
-        "<E'> -> +<T><E''> / +",
-        "<T'> -> *<F><T''> / *",
-        "<E'1> -> e / #",
-        "<E'1> -> <E'> / +",
-        "<T'1> -> e / +, #",
-        "<T'1> -> <T'> / *",
-        "<E''> -> e / #",
-        "<E''> -> <E'> / +",
-        "<T''> -> e / +, #",
-        "<T''> -> <T'> / *",
+        '<S> -> <E># / id, -, (',
+        '<E> -> <T><E\'1> / id, -, (',
+        '<T> -> <F><T\'1> / id, -, (',
+        '<F> -> id / id',
+        '<F> -> -<F> / -',
+        '<F> -> (<F>) / (',
+        '<E\'> -> +<T><E\'\'> / +',
+        '<T\'> -> *<F><T\'\'> / *',
+        '<E\'1> -> e / #',
+        '<E\'1> -> <E\'> / +',
+        '<T\'1> -> e / +, #',
+        '<T\'1> -> <T\'> / *',
+        '<E\'\'> -> e / #',
+        '<E\'\'> -> <E\'> / +',
+        '<T\'\'> -> e / +, #',
+        '<T\'\'> -> <T\'> / *',
     ]
-
-    // const grammar = indexGrammar(grammarSample.join('\n'))
-    // console.log(grammar)
 
     const table = parseGrammar(grammarSample)
     console.log(table)
